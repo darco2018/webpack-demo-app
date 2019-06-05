9f10e950acc3d37d81b93c8260d1a7970382b967
darco2018 <newsecond@outlook.com>
2019-06-05 14:35:33 +0200

    Move .js to dist/js

--- webpack.prod.js
+++ webpack.prod.js
@@ -10,7 +10,7 @@ var HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = merge(common, {
   mode: "production",
   output: {
-    filename: "[name].[contentHash].bundle.js",
+    filename: path.join("javascripts", "[name].[contentHash].bundle.js"),
     path: path.resolve(__dirname, "dist")
   },
   optimization: {
